30678ba170e3b1f170fc7f096d07ef0b5a3ce48d
Danya Generalov <danya@danya02.ru>
2024-04-09 17:19:27 +0300

    Adding basic video control listening.

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,46 @@
+use shadow_clone::shadow_clone;
+use web_sys::wasm_bindgen::JsCast;
+use web_sys::HtmlVideoElement;
 use yew::prelude::*;
 
 #[function_component]
 fn App() -> Html {
-    let counter = use_state(|| 0);
-    let onclick = {
-        let counter = counter.clone();
-        move |_| {
-            let value = *counter + 1;
-            counter.set(value);
+    let is_playing = use_state(|| false);
+    let current_time = use_state(|| 0.0);
+
+    let ontimeupdate = Callback::from({
+        shadow_clone!(current_time);
+        move |ev: Event| {
+            let element: HtmlVideoElement = ev.target().unwrap().dyn_into().unwrap();
+            current_time.set(element.current_time());
         }
-    };
+    });
+
+    let onplay = Callback::from({
+        shadow_clone!(is_playing);
+        move |_ev: Event| {
+            is_playing.set(true);
+        }
+    });
+    let onpause = Callback::from({
+        shadow_clone!(is_playing);
+        move |_ev: Event| {
+            is_playing.set(false);
+        }
+    });
 
     html! {
-        <div>
-            <button {onclick}>{ "+1" }</button>
-            <p>{ *counter }</p>
+        <div class="container">
+            <video src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" controls={true}
+            {ontimeupdate} {onplay} {onpause}/>
+            <hr />
+            <p>{"Current time: "}{*current_time}</p>
+            <p>{"Is playing: "}{*is_playing}</p>
         </div>
     }
 }
 
 fn main() {
+    wasm_logger::init(wasm_logger::Config::default());
     yew::Renderer::<App>::new().render();
 }
