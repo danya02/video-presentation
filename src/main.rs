af90c6319959eb13219ced19014e1917bef5ef45
Danya Generalov <danya@danya02.ru>
2024-04-10 11:30:36 +0300

    Switching to struct component and changing rate computation.

--- src/main.rs
+++ src/main.rs
@@ -1,192 +1,279 @@
 use std::time::Duration;
 
-use shadow_clone::shadow_clone;
-use wasm_bindgen::{closure::Closure, convert::IntoWasmAbi, JsValue};
-use web_sys::wasm_bindgen::JsCast;
+use gloo::{events::EventListener, timers::callback::Interval};
+use wasm_bindgen::prelude::*;
 use web_sys::HtmlVideoElement;
+use web_sys::{js_sys::wasm_bindgen, wasm_bindgen::JsCast};
 use webvtt::{Block, Cue};
 use yew::prelude::*;
-use yew_hooks::{use_interval, use_is_first_mount, use_state_ptr_eq};
 
-#[function_component]
-fn App() -> Html {
-    let subs = use_state(|| webvtt::parse_file(include_str!("../media/subs.vtt")).unwrap());
-    let current_block = use_state_eq(|| (0usize, false));
-    let is_playing = use_state_eq(|| false);
-    let current_time = use_state(|| Duration::from_secs(0));
-    let current_rate = use_state(|| 1.0);
-    let high_res_callback = use_state(|| None);
-    let video_el = use_node_ref();
-    let deadline_block_idx = use_state(|| 0usize);
+struct App {
+    subs: webvtt::File,
+    current_block: usize,
+    current_block_has_passed: bool,
+    is_playing: bool,
+    current_time: Duration,
+    current_rate: f64,
+    high_res_callback: Option<Closure<dyn FnMut(JsValue, JsValue)>>,
+    video_el: NodeRef,
+    deadline_block_idx: usize,
+    global_keydown_listener: Option<EventListener>,
+    interval_callback: Option<Interval>,
+    block_timing_history: Vec<f64>,
+    current_block_started_at: f64,
+    target_rate: f64,
+}
 
-    fn b2c(b: &Block) -> &Cue {
-        match b {
-            Block::Cue(v) => v,
-        }
-    }
+#[wasm_bindgen]
+extern "C" {
+    fn unixtime() -> f64;
+}
 
-    fn fits(time: Duration, cue: &Cue) -> bool {
-        return cue.start <= time && time <= cue.end;
-    }
+enum Msg {
+    Periodic,
+    Playing(bool),
+    RateChange,
+    NextDeadline,
+}
 
-    let onratechange = Callback::from({
-        shadow_clone!(current_rate);
-        move |ev: Event| {
-            let element: HtmlVideoElement = ev.target().unwrap().dyn_into().unwrap();
-            current_rate.set(element.playback_rate());
-        }
-    });
+impl Component for App {
+    type Message = Msg;
 
-    let ontimeupdate = Callback::from({
-        shadow_clone!(
-            current_time,
-            current_block,
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        let text = include_str!("../media/subs-verbose.vtt");
+        // log::info!("{text}");
+        let subs = webvtt::parse_file(text).unwrap();
+        Self {
             subs,
-            video_el,
-            deadline_block_idx
-        );
-        move |ev: Event| {
-            let subs = &*subs;
-            let element: HtmlVideoElement;
-            if let Some(v) = video_el.get() {
-                element = v.dyn_into().unwrap();
-            } else {
-                element = ev.target().unwrap().dyn_into().unwrap();
-            }
-            let now = Duration::from_secs_f64(element.current_time());
-            current_time.set(now);
-
-            let sub_list = &(*subs.blocks);
-            let (mut current_block_idx, mut current_block_is_visible) = *current_block;
-
-            // Loop over the blocks to find one that the value matches.
-            let idxs = (current_block_idx..sub_list.len()).chain(0..current_block_idx);
-            for idx in idxs {
-                let cue = b2c(&sub_list[idx]);
-                // If this cue fits, set this as the current block.
-                if fits(now, cue) {
-                    current_block_idx = idx;
-                    current_block_is_visible = true;
-                    break;
-                }
-                // Otherwise, if now is after the cue,
-                // then this cue is the last visible one.
-                if cue.end < now {
-                    current_block_idx = idx.max(current_block_idx);
-                    current_block_is_visible = false;
-                }
-            }
-
-            current_block.set((current_block_idx, current_block_is_visible));
-
-            // Set the playback rate based on the time left until the end of the deadline block.
-            let deadline_block = b2c(&sub_list[*deadline_block_idx]);
-            let time_until_end = deadline_block.end.checked_sub(now).unwrap_or_default();
-
-            let rate_fn = |time: Duration| {
-                let time_s = time.as_secs_f64();
-                let k = 2.0;
-                2.0 * (1.0 / (1.0 + std::f64::consts::E.powf(-k * time_s)) - 0.5)
-            };
-            element.set_playback_rate(rate_fn(time_until_end));
+            current_block: 0,
+            current_block_has_passed: false,
+            is_playing: false,
+            current_time: Duration::ZERO,
+            current_rate: 1.0,
+            high_res_callback: None,
+            video_el: NodeRef::default(),
+            deadline_block_idx: 0,
+            global_keydown_listener: None,
+            interval_callback: None,
+            current_block_started_at: 0.0,
+            block_timing_history: vec![],
+            target_rate: 1.0,
         }
-    });
+    }
 
-    let advance_deadline_block = {
-        shadow_clone!(deadline_block_idx);
-        Callback::from(move |ev: MouseEvent| {
-            log::info!("Advancing deadline block idx: was {}", *deadline_block_idx);
-            deadline_block_idx.set(*deadline_block_idx + 1);
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        let ontimeupdate = ctx.link().callback(|_ev| Msg::Periodic);
+        let onplay = ctx.link().callback(|_ev| Msg::Playing(true));
+        let onpause = ctx.link().callback(|_ev| Msg::Playing(false));
+        let onratechange = ctx.link().callback(|_ev| Msg::RateChange);
+        let advance_deadline_block = ctx.link().callback(|ev: MouseEvent| {
             ev.prevent_default();
-        })
-    };
+            Msg::NextDeadline
+        });
 
-    use_interval(
-        {
-            shadow_clone!(ontimeupdate);
-            move || ontimeupdate.emit(Event::new("none").unwrap())
-        },
-        100,
-    );
+        html! {
+            <div class="container">
+                <video src="/media/vid.webm" controls={true} ref={self.video_el.clone()} muted={true}
+                {ontimeupdate} {onplay} {onpause} {onratechange}
+                style="width: 100%;"/>
 
-    let is_first = use_is_first_mount();
-    let global_keydown_handler = use_state_ptr_eq(|| None);
-    if is_first {
-        let window = web_sys::window().unwrap();
-        let listener = gloo::events::EventListener::new(&window, "keydown", {
-            shadow_clone!(advance_deadline_block);
-            move |e| {
-                let e: KeyboardEvent = (e.clone()).dyn_into().unwrap();
-                let keycode = e.key_code();
-                log::info!("Pressed key {keycode}");
-                if keycode == 33 {
-                    // page up (prev)
-                } else if keycode == 34 {
-                    // page down (next)
-                    log::info!("Sending advance event");
-                    advance_deadline_block.emit(MouseEvent::new("none").unwrap());
+                <hr />
+                <p>{"Current time: "}{format!("{:?}", self.current_time)}</p>
+                <p>{"Current playback rate: "}{self.current_rate}</p>
+                <p>{"Is playing: "}{self.is_playing}</p>
+                <p>{"Current block: "}{format!("{:?}", &(self.subs.blocks[self.current_block]))}</p>
+                <p>{"Current block is visible: "}{self.current_block_has_passed}</p>
+                <p>{"Duration history: "}{format!("{:?}", self.block_timing_history)}</p>
+                <button class="btn btn-success" onclick={advance_deadline_block}>{"Advance deadline..."}</button>
+            </div>
+        }
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::Periodic => {
+                self.periodic();
+            }
+            Msg::Playing(pl) => {
+                self.is_playing = pl;
+                if pl {
+                    self.current_block_started_at = unixtime();
                 }
             }
-        });
-        global_keydown_handler.set(Some(listener));
-    }
-
-    if high_res_callback.is_none() {
-        #[wasm_bindgen::prelude::wasm_bindgen]
-        extern "C" {
-            fn request_video_frame_callback(
-                this: &HtmlVideoElement,
-                cb: &Closure<dyn FnMut(JsValue, JsValue)>,
-            );
-            fn request_video_frame_callback_again();
-
-        }
-
-        match video_el.cast::<HtmlVideoElement>() {
-            Some(el) => {
-                shadow_clone!(ontimeupdate);
-                let cb = Closure::new(move |now, metadata| {
-                    log::info!("Frame!");
-                    ontimeupdate.emit(Event::new("none").unwrap());
-                    request_video_frame_callback_again();
-                });
-                request_video_frame_callback(&el, &cb);
-                high_res_callback.set(Some(cb));
+            Msg::RateChange => {
+                self.current_rate = self
+                    .video_el
+                    .cast::<HtmlVideoElement>()
+                    .unwrap()
+                    .playback_rate();
             }
-            None => {}
+            Msg::NextDeadline => {
+                let deadline_cue = b2c(&self.subs.blocks[self.deadline_block_idx]);
+                self.deadline_block_idx += 1;
+                let elapsed = unixtime() - self.current_block_started_at;
+                self.current_block_started_at = unixtime();
+                let true_duration = (deadline_cue.end - deadline_cue.start).as_secs_f64();
+                log::info!(
+                    "Latest block was read in {elapsed}, but was supposed to take {true_duration}"
+                );
+                self.block_timing_history.push(true_duration / elapsed);
+
+                // Calculate the target rate by averaging the previous rates.
+                self.target_rate = self.block_timing_history.iter().cloned().sum::<f64>()
+                    / self.block_timing_history.len() as f64;
+            }
         }
+        true
     }
 
-    let onplay = Callback::from({
-        shadow_clone!(is_playing);
-        move |_ev: Event| {
-            is_playing.set(true);
+    fn rendered(&mut self, ctx: &Context<Self>, first_render: bool) {
+        if first_render {
+            // Here we'll set up the global event listener
+            let advance_deadline_block = ctx.link().callback(|_| Msg::NextDeadline);
+
+            let window = web_sys::window().unwrap();
+            let listener = EventListener::new(&window, "keydown", {
+                move |e| {
+                    let e: KeyboardEvent = (e.clone()).dyn_into().unwrap();
+                    let keycode = e.key_code();
+                    log::info!("Pressed key {keycode}");
+                    if keycode == 33 {
+                        // page up (prev)
+                    } else if keycode == 34 {
+                        // page down (next)
+                        log::info!("Sending advance event");
+                        advance_deadline_block.emit(());
+                    }
+                }
+            });
+            self.global_keydown_listener = Some(listener);
+
+            // Also set up the global interval
+            let periodic = ctx.link().callback(|_| Msg::Periodic);
+            self.interval_callback = Some(Interval::new(100, move || {
+                periodic.emit(());
+            }));
+
+            // Also set up the high-resolution callback.
+            #[wasm_bindgen]
+            extern "C" {
+                fn request_video_frame_callback(
+                    this: &HtmlVideoElement,
+                    cb: &Closure<dyn FnMut(JsValue, JsValue)>,
+                );
+                fn request_video_frame_callback_again();
+
+            }
+
+            let el = self.video_el.cast::<HtmlVideoElement>().unwrap();
+            let periodic = ctx.link().callback(|_| Msg::Periodic);
+            let cb = Closure::new(move |now, metadata| {
+                periodic.emit(());
+                request_video_frame_callback_again();
+            });
+            request_video_frame_callback(&el, &cb);
+            self.high_res_callback = Some(cb);
+
+            // Also mute the video.
+            el.set_muted(true);
         }
-    });
-    let onpause = Callback::from({
-        shadow_clone!(is_playing);
-        move |_ev: Event| {
-            is_playing.set(false);
-        }
-    });
-
-    let subs = &*subs;
-
-    html! {
-        <div class="container">
-            <video src="/media/vid.webm" controls={true} ref={video_el} muted={true}
-            {ontimeupdate} {onplay} {onpause} {onratechange}
-            style="width: 100%;"/>
-
-            <hr />
-            <p>{"Current time: "}{format!("{:?}", *current_time)}</p>
-            <p>{"Current playback rate: "}{*current_rate}</p>
-            <p>{"Is playing: "}{*is_playing}</p>
-            <p>{"Current block: "}{format!("{:?}", &(subs.blocks[(*current_block).0]))}</p>
-            <p>{"Current block is visible: "}{(*current_block).1}</p>
-            <button class="btn btn-success" onclick={advance_deadline_block}>{"Advance deadline..."}</button>
-        </div>
     }
+}
+
+impl App {
+    fn periodic(&mut self) {
+        let element: HtmlVideoElement;
+        if let Some(v) = self.video_el.get() {
+            element = v.dyn_into().unwrap();
+        } else {
+            return;
+        }
+        let now = Duration::from_secs_f64(element.current_time());
+        self.current_time = now;
+
+        let sub_list = &self.subs.blocks;
+
+        // Loop over the blocks to find one that the value matches.
+        let idxs = (self.current_block..sub_list.len()).chain(0..self.current_block);
+        for idx in idxs {
+            let cue = b2c(&sub_list[idx]);
+            // If this cue fits, set this as the current block.
+            if fits(now, cue) {
+                self.current_block = idx;
+                self.current_block_has_passed = false;
+                break;
+            }
+            // Otherwise, if now is after the cue,
+            // then this cue is the last visible one.
+            if cue.end < now {
+                self.current_block = idx.max(self.current_block);
+                self.current_block_has_passed = true;
+            }
+        }
+
+        // Set the playback rate based on the time left until the end of the deadline block.
+        let deadline_block = b2c(&sub_list[self.deadline_block_idx]);
+        let time_until_end = deadline_block.end.checked_sub(now).unwrap_or_default();
+        let near_curve =
+            bezier_rs::Bezier::from_cubic_coordinates(0.0, 0.0, 0.0, 0.25, 1.0, 0.0, 1.0, 1.0);
+        let far_curve =
+            bezier_rs::Bezier::from_cubic_coordinates(1.0, 1.0, 1.6, 1.0, 2.0, 1.5, 2.0, 2.0);
+
+        let rate_fn = |time: Duration| {
+            let time_s = time.as_secs_f64();
+            let deadline_block_duration = (deadline_block.end - deadline_block.start).as_secs_f64();
+            let x = time_s / deadline_block_duration;
+            let advanced_rate = |x, target_rate| {
+                let k = 5.0;
+                2.0 * target_rate * (1.0 / (1.0 + std::f64::consts::E.powf(-k * x)) - 0.5)
+            };
+
+            // If the current block on the screen is not the current deadline block, we seek fast to it.
+            if self.current_block != self.deadline_block_idx {
+                return 2.0 * (time_s / deadline_block_duration) * self.target_rate;
+            }
+
+            if x > 1.0 {
+                far_curve
+                    .evaluate(bezier_rs::TValue::Parametric((x - 1.0).min(1.0)))
+                    .y
+                    * self.target_rate
+            } else {
+                advanced_rate(x, self.target_rate)
+            }
+
+            // if x > 1.0 {
+            //     return x * advanced_rate(2.0, 1.0);
+            // } else {
+            //     advanced_rate(x, self.target_rate)
+            // }
+            // if x > 1.0 {
+            //     2.0
+            // } else {
+            //     advanced_rate(x, self.target_rate)
+            // }
+        };
+
+        #[wasm_bindgen]
+        extern "C" {
+            fn try_set_playback_rate(el: &HtmlVideoElement, rate: f64) -> bool;
+        }
+        if !try_set_playback_rate(&element, rate_fn(time_until_end)) {
+            // Playback rate was bad, but we can ignore that.
+        }
+    }
+}
+
+fn b2c(b: &Block) -> &Cue {
+    match b {
+        Block::Cue(v) => v,
+    }
+}
+
+fn fits(time: Duration, cue: &Cue) -> bool {
+    return cue.start <= time && time <= cue.end;
 }
 
 fn main() {
